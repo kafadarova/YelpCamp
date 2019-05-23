f1d7f156006ee64b67e0a5175a82dea5ede77a19
kafadarova <k.kafadarova@gmail.com>
2019-05-23 16:55:34 +0200

    Add a new campground and image for it

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
+let campgrounds = [
+  {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+  {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}   
+]
+
 // render the landing page from views landing template
 app.get('/',(req,res) => {
   res.render('landing');
@@ -12,19 +18,23 @@ app.get('/',(req,res) => {
 
 // camp ground route
 app.get('/campgrounds', (req,res) => {
-  let campgrounds = [
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}   
-  ]
   // campgrounds (name - call wharever we want): campgrounds (data)
   res.render('campgrounds', {campgrounds: campgrounds});
 });
 
+// show the form that will send data to the post campgrounds
+app.get('/campgrounds/new', (req,res) => {
+  res.render('new.ejs');
+});
+
 app.post('/campgrounds', (req,res) => {
-  res.send('You hit the post route');
   // get data from form and add to campground array
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
   // redirect back to campgrounds page
+  res.redirect('campgrounds');
 });
 
 // use port 3000 unless there exists a preconfigured port
