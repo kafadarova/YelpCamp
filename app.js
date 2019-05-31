ac390d4d783310daaa3ffa1ecacc8d59026ba284
kafadarova <k.kafadarova@gmail.com>
2019-05-31 13:00:07 +0200

    adding a page to show each campground

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set('view engine', 'ejs');
 // schema setup
 const campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 // make a model 
@@ -19,7 +20,8 @@ const Campground = mongoose.model('Campground', campgroundSchema);
 // Campground.create(
 //   {
 //     name: "Granite Hill", 
-//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
 //   }, function(err, campground) {
 //     if (err) {
 //       console.log(err);
@@ -42,16 +44,10 @@ app.get('/campgrounds', (req,res) => {
         console.log(err);
       } else {
         // campgrounds (name - call wharever we want): campgrounds (data)
-        res.render('campgrounds', {campgrounds: allCampgrounds});
+        res.render('index', {campgrounds: allCampgrounds});
       }
     })
 });
-
-// NEW route - show the form that will send data to the post campgrounds
-app.get('/campgrounds/new', (req,res) => {
-  res.render('new.ejs');
-});
-
 // CREATE route- add new campground
 app.post('/campgrounds', (req,res) => {
   // get data from form and add to campground array
@@ -69,6 +65,24 @@ app.post('/campgrounds', (req,res) => {
     }
   });
 });
+
+// NEW route - show the form that will send data to the post campgrounds
+app.get('/campgrounds/new', (req,res) => {
+  res.render('new.ejs');
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err)
+        }else{
+            //render show template with that campground
+             res.render("show",{campground: foundCampground}); 
+        }
+    });
+})
 
 // use port 3000 unless there exists a preconfigured port
 const port = process.env.port || 3001;
