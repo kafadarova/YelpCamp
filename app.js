ecc0a41a39b87e39f5f2557484bab724907285d7
kafadarova <k.kafadarova@gmail.com>
2019-06-06 23:03:40 +0200

    create a comment form and  routes for the comments

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
-const express    = require('express'),
-      app        = express(),
-      bodyParser = require('body-parser'),
-      mongoose   = require('mongoose'),
-      Campground = require('./models/campground'),
-      seedDB     = require('./seeds');
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  Campground = require('./models/campground'),
+  seedDB = require('./seeds');
 
 // connect mongoose
-mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
-app.use(bodyParser.urlencoded({extended : true}));
+mongoose.connect('mongodb://localhost/yelp_camp', {
+  useNewUrlParser: true
+});
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'ejs');
 
 // remove all campgrounds from the db
 seedDB();
 
 // render the landing page from views landing template
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.render('landing');
 });
 
 // INDEX campground route - showw all camp ground
-app.get('/campgrounds', (req,res) => {
+app.get('/campgrounds', (req, res) => {
   //  Get alll campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-      if (err) {
-        console.log(err);
-      } else {
-        // campgrounds (name - call wharever we want): campgrounds (data)
-        res.render('index', {campgrounds: allCampgrounds});
-      }
-    })
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      // campgrounds (name - call wharever we want): campgrounds (data)
+      res.render('campgrounds/index', {
+        campgrounds: allCampgrounds
+      });
+    }
+  })
 });
 // CREATE route- add new campground
-app.post('/campgrounds', (req,res) => {
+app.post('/campgrounds', (req, res) => {
   // get data from form and add to campground array
   let name = req.body.name;
   let image = req.body.image;
   let desc = req.body.description;
-  let newCampground = {name: name, image: image, description: desc};
+  let newCampground = {
+    name: name,
+    image: image,
+    description: desc
+  };
   // campgrounds.push(newCampground);
   // Create a new campground and save to DB
   Campground.create(newCampground, (err, newlyCreated) => {
@@ -50,27 +60,45 @@ app.post('/campgrounds', (req,res) => {
 });
 
 // NEW route - show the form that will send data to the post campgrounds
-app.get('/campgrounds/new', (req,res) => {
-  res.render('new.ejs');
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
 });
 
 // SHOW - shows more info about one campground
-app.get("/campgrounds/:id", function(req, res){
-    //find the campground with provided ID
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if(err){
-            console.log(err)
-        }else{
-          console.log(foundCampground);
-            //render show template with that campground
-             res.render("show",{campground: foundCampground}); 
-        }
-    });
+app.get("/campgrounds/:id", function(req, res) {
+  //find the campground with provided ID
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(foundCampground);
+      //render show template with that campground
+      res.render("campgrounds/show", {
+        campground: foundCampground
+      });
+    }
+  });
 });
 
+// ========
+// Comments routers
+// ========
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+  // find campground by id
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', {
+        campground: campground
+      })
+    }
+  })
+})
 // use port 3000 unless there exists a preconfigured port
 const port = process.env.port || 3001;
 
-app.listen(port,() => {
-   console.log('The YelpCamp Server Has Started!');
+app.listen(port, () => {
+  console.log('The YelpCamp Server Has Started!');
 });
