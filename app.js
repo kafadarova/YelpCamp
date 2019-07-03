409ca488298524fc5530d48bdcbea81e14fd92b8
kafadarova <k.kafadarova@gmail.com>
2019-07-03 18:04:50 +0200

    flash messages- success and error

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req,res, next) => {
   // what is available inside the template
     res.locals.currentUser = req.user;
-    res.locals.message = req.flash('error');
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
     next();
 });
 
