0ccb5bbfd96ad32904043a2be4c2ce6c245f4eb5
kafadarova <k.kafadarova@gmail.com>
2019-07-23 22:15:15 +0200

    add a function to calculate the average review

--- routes/reviews.js
+++ routes/reviews.js
@@ -59,3 +59,14 @@ router.post("/", middleware.isLoggedIn, middleware.checkReviewExistence, functio
         });
     });
 });
+
+function calculateAverage(reviews) {
+    if (reviews.length === 0) {
+        return 0;
+    }
+    var sum = 0;
+    reviews.forEach(function (element) {
+        sum += element.rating;
+    });
+    return sum / reviews.length;
+}
